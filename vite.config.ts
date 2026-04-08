09bd91596e30bf738ed2b0c816363fe4dddac2de
Kelechi Egbuta <keleGod.co@gmail.com>
2026-04-08 18:32:25 +0100

    feat: integrate Netlify plugins and configure build settings
    - Added @netlify/vite-plugin and @netlify/vite-plugin-react-router to vite.config.ts for enhanced routing support.
    - Created netlify.toml to define build and development commands for React Router.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+import netlifyReactRouter from '@netlify/vite-plugin-react-router'
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
+    netlify(),
     tailwindcss(),
     reactRouter(),
+    netlifyReactRouter(),
   ],
   resolve: {
     alias: {
